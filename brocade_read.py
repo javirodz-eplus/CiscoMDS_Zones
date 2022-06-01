004cfe1fa6402d0f6e856b797411fb97b7416bd8
javirodz-eplus <javier.rodriguez@eplus.com>
2022-06-01 17:58:24 -0400

    With optional brocade_read.py. This python script will attempt to read a 'showzone' command from brocade to create the input.json needed to create the zoning.txt

--- brocade_read.py
+++ brocade_read.py
@@ -6,11 +6,15 @@ import json
 
 alias = {}
 reversealias = {}
+zones = {}
+zonelines = []
 
 if os.path.exists('zoneshow.txt'):
     zone_conf_file = open('zoneshow.txt', 'r')
 zonefile = zone_conf_file.readlines()
 zone_conf_file.close()
+iseffective = False
+# aliases
 lineNumber = 0
 for line in zonefile:
     currentline = line.strip()
@@ -18,8 +22,17 @@ for line in zonefile:
     if currentline.startswith("alias"):
         alias[currentline.split(" ")[1]] = zonefile[lineNumber].strip()
         reversealias[zonefile[lineNumber].strip()] = currentline.split(" ")[1]
-print(alias)
-print(reversealias)
+    # Effort to get the current effective zones up to line 35
+    elif currentline.startswith("zone"):
+        if iseffective:
+            zonelines.append(lineNumber)
+            print(currentline)
+            try:
+                print(reversealias[zonefile[lineNumber].strip()])
+            except:
+                pass
+    elif currentline.startswith("Effective configuration"):
+        iseffective = True
 
 print(f'The WWPN for initiator CHHPure01_CT0_FC0 is => ', alias['CHHPure01_CT0_FC0'])
 print(f'The initiator with WWPN 52:4a:93:72:e5:fb:c5:00 is => ', reversealias['52:4a:93:72:e5:fb:c5:00'])
@@ -28,3 +41,6 @@ with open('alias.json', 'w') as fp1:
     json.dump(alias, fp1)
 with open('reversealias.json', 'w') as fp2:
     json.dump(reversealias, fp2)
+
+# zones
+print(zonelines)
